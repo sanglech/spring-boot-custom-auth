8de69f637fb3d72533600f8ba558ffe43329ca30
Christian <christian@sanglech.com>
2021-05-05 16:16:56 +0900

    Initial commit

---
+++ src/main/java/com/christian/employeedemoauthjpa/EmployeeDemoAuthJpaApplication.java
@@ -0,0 +1,13 @@
+package com.christian.employeedemoauthjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDemoAuthJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDemoAuthJpaApplication.class, args);
+	}
+
+}
