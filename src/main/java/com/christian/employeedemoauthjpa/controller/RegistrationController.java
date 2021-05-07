3e36d4ca9918c41d0e4e8c940a3f1b14217de414
Christian <christian@sanglech.com>
2021-05-08 00:55:19 +0900

    Added validation for registration form

--- src/main/java/com/christian/employeedemoauthjpa/controller/RegistrationController.java
+++ src/main/java/com/christian/employeedemoauthjpa/controller/RegistrationController.java
@@ -1,16 +1,17 @@
 package com.christian.employeedemoauthjpa.controller;
 
+import com.christian.employeedemoauthjpa.entity.User;
 import com.christian.employeedemoauthjpa.service.UserService;
 import com.christian.employeedemoauthjpa.user.CrmUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.logging.Logger;
 
 @Controller
@@ -35,6 +36,36 @@ public class RegistrationController {
 
         theModel.addAttribute("crmUser",new CrmUser());
         return "registration-form";
+    }
+
+    @PostMapping("/processRegistrationForm")
+    public String processRegistrationForm(@Valid @ModelAttribute("crmUser") CrmUser theCrmUser,
+                                          BindingResult theBindingResult,
+                                          Model theModel){
+        String userName = theCrmUser.getUserName();
+        logger.info("Processing registration form for: " + userName);
+
+        // form validation
+        if (theBindingResult.hasErrors()){
+            return "registration-form";
+        }
+
+        // check the database if user already exists
+        User existing = userService.findByUserName(userName);
+        if (existing != null){
+            theModel.addAttribute("crmUser", new CrmUser());
+            theModel.addAttribute("registrationError", "User name already exists.");
+
+            logger.warning("User name already exists.");
+            return "registration-form";
+        }
+
+        // create user account
+        //userService.save(theCrmUser);
+
+        logger.info("Successfully created user: " + userName);
+
+        return "registration-complete";
     }
 
 }
--- src/main/java/com/christian/employeedemoauthjpa/user/CrmUser.java
+++ src/main/java/com/christian/employeedemoauthjpa/user/CrmUser.java
@@ -3,6 +3,7 @@ package com.christian.employeedemoauthjpa.user;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
 public class CrmUser {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
